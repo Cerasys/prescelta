0a85ed6d8bed2082cff4bb32dd54b102c53349e7
Cerasys <nathanblee@gmail.com>
2024-03-11 16:46:55 -0700

    calendly implementation

--- src/components/calendly/calendly.js
+++ src/components/calendly/calendly.js
@@ -2,17 +2,24 @@ import React from "react";
 
 const Calendly = () => {
   return (
-    <div>
-      <div
-        class="calendly-inline-widget"
-        data-url="https://calendly.com/prescelta/private-practice-goal-setting"
-        style={{ minWidth: "320px", height: "700px" }}
-      ></div>
-      <script
-        type="text/javascript"
-        src="https://assets.calendly.com/assets/external/widget.js"
-        async
-      ></script>
+    <div id="calendly">
+      <div className="container">
+        <div className="col-md-12">
+          <div className="row"></div>
+          <div>
+            <div
+              class="calendly-inline-widget"
+              data-url="https://calendly.com/prescelta/private-practice-goal-setting"
+              style={{ minWidth: "320px", height: "100%" }}
+            ></div>
+            <script
+              type="text/javascript"
+              src="https://assets.calendly.com/assets/external/widget.js"
+              async
+            ></script>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
