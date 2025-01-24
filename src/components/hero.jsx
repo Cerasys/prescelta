a381fa9bc0cceb076150444edd10720d491afd8b
Nathan Lee <nathanblee@gmail.com>
2025-01-23 16:24:45 -0800

    hero changes

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -8,20 +8,20 @@ export const Hero = () => {
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                <h4>High Ticket Coaches...</h4>
+                <h4>Clothing Brands...</h4>
                 <h1>
-                  DO YOU WANT{" "}
-                  <span className="highlight">MORE COACHING CLIENTS?</span>
+                  DO YOU WANT TO BE{" "}
+                  <span className="highlight">A BRAND PEOPLE REMEMBER?</span>
                   <span></span>
                 </h1>
                 <br />
                 <br />
                 <p>
-                  Use our one-click system that generates high ticket,
-                  knowledge-hungry customers, on autopilot.
+                  We generate hungry customers that want to part of your brand
+                  and your story with our one-click system.
                 </p>
                 <a
-                  href="https://calendly.com/nathanblee/growth-blueprint-call"
+                  href="https://go.hoshinomedia.com/book-call"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="btn btn-custom btn-lg page-scroll"
