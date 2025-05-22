1ff0d5e1d0c8a4e771061e1c5fc33df569a943da
Nathan <nathanblee@gmail.com>
2025-05-22 16:24:13 -0700

    changed about section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -16,33 +16,24 @@ export const About = () => {
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
               <h2>Our Story</h2>
-              <h4>How do you go about scaling? </h4>
-              <p>
-                I'll give you a hint: it comes with time and perseverence and a
-                whole lot of sweat equity...
-                <br />
-                <b>
-                  But it's only doable if you have a few key systems in place
-                </b>
-                <br />
-              </p>
               <p>
                 I'm Nathan, your{" "}
-                <b>sales process architect and marketing wizard</b>. Having got
-                my start in e-commerce, I've cut my teeth on marketing,{" "}
-                <u>first for survival, then for fun.</u>
+                <b>sales process architect and marketing wizard</b>. I cut my
+                teeth in ecommerce, <u>first for survival, then for fun.</u>
                 <br />
                 <br />
-                In 2020, I started out in ecommerce and got{" "}
-                <i>dropkicked by Covid.</i>
+                In 2020, I started out and got <i>dropkicked by Covid.</i>
                 <br />
                 <br />
-                I was making organic content but barely seeing any progress. I
-                knew I needed to advertise, but I had no idea how.
+                I was making organic content but barely saw any progress.
+                <br /> I knew I needed to market, but had no idea how.
                 <br />
                 <br />
-                I wasted my first $300 on advertisements, with one sale.
-                <br />I wasted the next $300. <b>Two sales. Progress.</b>
+                I wasted my first $300 on paid ads, with one sale.
+                <br />I wasted the next $300, no sales.
+                <br />
+                We added on UGC and user testimonials. Two sales.{" "}
+                <b>Progress.</b>
                 <br />
                 <br />I kept cranking until from zero until I saw{" "}
                 <b>
@@ -55,41 +46,23 @@ export const About = () => {
                 friends, and then for strangers brought to me by the universe.
                 <br />
                 <br />
-                Since then, me and my partner have{" "}
-                <u>
-                  spent over 7 figures in ads, sent over 600,000 texts, and over
-                  1 MILLION emails
-                </u>
-                , to drive business for brands, and I'm here to help you next.
-              </p>
-              <p>
+                Influence is a funny thing.
+                <br />{" "}
                 <b>
-                  My team has built our revolutionary "TBF System" that has
-                  decreased customer costs by more than 5x for hungry customers
-                  that want to be part of your tribe.
+                  We all have people in our life who have impact on our lives...{" "}
                 </b>
                 <br />
-              </p>
-              <p>
-                And me and my team want to partner with you to bring you more
-                sales and experience more freedom in your life.
-              </p>
-              <p>
-                So stop paying posting on TikTok and Instagram praying that
-                you'll go viral.
+                people we pay attention to, who hold weight in their opinions
+                and emotions.
                 <br />
-                Stop trying to pay influencers to make your brand look more
-                credible.
-                <br />
-                <b>
-                  Let us develop you a custom, automated customer acquisition
-                  method to help you crush your business goals...
-                </b>
+                <br />I want to show you how Influencer Marketing is more than
+                just a channel, <b>it's an engine for growth.</b>
                 <br />
               </p>
               <h3>
-                So if you want an automated, scalable way to tell your brand's
-                story to the world...
+                So if you're ready to leverage Influencer Marketing to help your
+                Organic, Paid Media, Retail, Customer Experience, and Your
+                Bottom Line...
               </h3>
               <a
                 href="https://apply.hoshinomedia.com/apply"
