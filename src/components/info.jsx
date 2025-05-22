de13ba4ee13ceed67bbb70b9c40f7387868f1af4
Nathan <nathanblee@gmail.com>
2025-05-22 16:51:40 -0700

    updated footer

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -5,7 +5,7 @@ export const Info = (props) => {
     <div>
       <div id="contact">
         <div className="container">
-          <div className="col-md-8">
+          {/* <div className="col-md-8">
             <div className="row">
               <div className="section-title">
                 <h2>Get In Touch</h2>
@@ -27,47 +27,58 @@ export const Info = (props) => {
                 {props.data ? props.data.email : "loading"}
               </p>
             </div>
-          </div>
-          <div className="col-md-12">
-            <div className="row">
-              <div className="social">
-                <ul>
-                  <li>
-                    <a
-                      href={props.data ? props.data.facebook : "/"}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className="fa fa-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href={props.data ? props.data.youtube : "/"}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className="fa fa-youtube"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href={props.data ? props.data.instagram : "/"}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className="fa fa-instagram"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
+          </div> */}
+          <div className="row">
+            <div className="social">
+              <ul>
+                <li>
+                  <a
+                    href={props.data ? props.data.facebook : "/"}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href={props.data ? props.data.youtube : "/"}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fa fa-youtube"></i>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href={props.data ? props.data.instagram : "/"}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fa fa-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href={props.data ? props.data.linkedin : "/"}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fa fa-linkedin"></i>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href={props.data ? props.data.email : "/"}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="fa fa-envelope-o"></i>
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
-        </div>
-      </div>
-      <div id="footer">
-        <div className="container text-center">
-          <p>&copy; 2025 Prescelta LLC DBA Hoshino Media</p>
         </div>
       </div>
     </div>
