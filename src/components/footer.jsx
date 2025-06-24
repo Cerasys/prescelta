9248bd2cdf13ccdcdef84854d09bfa073277bbf0
Nathan <nathanblee@gmail.com>
2025-06-24 16:55:42 -0700

    fixed footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Info } from "./info";
+import "./footer.css";
+import JsonData from "../data/data.json";
 
-export const Footer = () => {
+export const Footer = (props) => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
   return (
     <div id="footer">
+      <Info data={landingPageData.Contact} />
+
       <div className="container text-center">
         <p>
           &copy; 2025 Prescelta LLC DBA Hoshino Media |{" "}
@@ -11,7 +21,7 @@ export const Footer = () => {
           </a>{" "}
           |{" "}
           <a href="/terms" target="_blank" rel="noopener noreferrer">
-            Terms and Conditions{" "}
+            Terms and Conditions
           </a>
         </p>
       </div>
--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,37 +1,14 @@
 import React from "react";
+import "./info.css";
 
 export const Info = (props) => {
   return (
     <div>
-      <div id="contact">
-        <div className="container">
-          {/* <div className="col-md-8">
-            <div className="row">
-              <div className="section-title">
-                <h2>Get In Touch</h2>
-              </div>
-              <a href="/book" target="_blank" rel="noopener noreferrer">
-                <div className="btn btn-custom btn-lg">book a call!</div>
-              </a>
-            </div>
-          </div>
-          <div className="col-md-3 col-md-offset-1 contact-info">
-            <div className="contact-item">
-              <h3>Contact Info</h3>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-envelope-o"></i> Email
-                </span>{" "}
-                {props.data ? props.data.email : "loading"}
-              </p>
-            </div>
-          </div> */}
-          <div className="row">
-            <div className="social">
-              <ul>
-                {/* <li>
+      <div className="container">
+        <div className="row">
+          <div className="social">
+            <ul>
+              {/* <li>
                   <a
                     href={props.data ? props.data.facebook : "/"}
                     target="_blank"
@@ -58,26 +35,25 @@ export const Info = (props) => {
                     <i className="fa fa-instagram"></i>
                   </a>
                 </li> */}
-                <li>
-                  <a
-                    href={props.data ? props.data.linkedin : "/"}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fa fa-linkedin"></i>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href={props.data ? props.data.email : "/"}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className="fa fa-envelope-o"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
+              <li>
+                <a
+                  href={props.data ? props.data.linkedin : "/"}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-linkedin"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href={props.data ? props.data.email : "/"}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-envelope-o"></i>
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
