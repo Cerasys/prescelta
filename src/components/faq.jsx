94b8dbdc7209840ee958c74f8bd0fd950956418a
Nathan <nathanblee@gmail.com>
2025-06-24 21:21:16 -0700

    faq

---
+++ src/components/faq.jsx
@@ -0,0 +1,141 @@
+import React from "react";
+import "./faq.css";
+
+export const FAQ = () => {
+  return (
+    <div id="faq">
+      <div className="faq container">
+        <div className="row">
+          <div className="about-text">
+            <h2>FAQ</h2>
+            <h5>for brands and creators</h5>
+            <h3>1. What is influencer marketing?</h3>
+            <p>
+              Influencer marketing is the greatest thing since canned bread.
+            </p>
+            <p>
+              Imagine having a small army of passionate, raving customers teling
+              their friends and family about your products.
+              <br />
+              <b>Except, they have 100,000 friends each.</b>
+            </p>
+            <p>
+              Every person has someone they look up to in their lives, someone
+              who they like and trust. <br /> That trust goes a long way to
+              giving them peace of mind that when they try a product, it'll be
+              all they dreamed of and more.
+            </p>
+            <p>
+              Not only that, brands can shortcut years of building out their
+              social media by partnering with creators who already know how to
+              tell a story.
+            </p>
+            <p>Nike had Michael Jordan.</p>
+            <p>Skims had Kim Kardashian.</p>
+            <p>Pepsi had Beyoncé.</p>
+            <p>
+              <b>Who will you have represent you to the next generation?</b>
+            </p>
+            <br />
+            <h3>
+              2. What sorts of influencer marketing services do you offer?
+            </h3>
+            <p>
+              We run managed campaigns, creator partnerships, talent management,
+              and more. Ask us about our scaling roadmap!
+            </p>
+            <br />
+            <h3>3. What makes Hoshino Media different from XYZ agency?</h3>
+            <p>
+              <b>💖 Creators are our soul 💖</b>
+            </p>
+            <p>
+              Hoshino Media focuses on representing the best storytellers, but
+              we don't just bring creators the big bucks and call it a day.
+            </p>
+            <p>
+              <u>To our creator clients, we're in it for the long haul:</u>
+              <ul>
+                <li>
+                  Not only will we find brand partners that align with your
+                  values, <br />
+                  <b>
+                    {"\t"}we'll help you define your career goals and build a
+                    roadmap to get there.
+                  </b>
+                </li>
+                <li>
+                  Not only will we negotiate better terms (usage, budget,
+                  timeline) for you, <br />
+                  <b>
+                    we'll help you analyze your audience to improve your content
+                    and connection to them.
+                  </b>
+                </li>
+                <li>
+                  Not only will we represent you and your audience to the brands
+                  we work with, <br />
+                  <b>we'll make you write thank you cards to them too. 📝</b>
+                </li>
+              </ul>
+            </p>
+            <p>
+              <u>
+                To our brand clients, you help us make all the creative process
+                possible:
+              </u>
+              <ul>
+                <li>
+                  Not only will we find creators that will say the script,{" "}
+                  <br />
+                  <b>
+                    {"\t"}we'll find creators who care and will help their
+                    audience to care too.
+                  </b>
+                </li>
+                <li>
+                  Not only will we maximize your ROI, <br />
+                  <b>
+                    we'll help you create experiences with your potential
+                    customers they will remember forever.
+                  </b>
+                </li>
+                <li>
+                  Not only will we wow you with campaign and results, <br />
+                  <b>you might also get a thank you cards too. 😂</b>
+                </li>
+              </ul>
+            </p>
+            <br />
+            <h3>4. What kind of creators do you work with?</h3>
+            <p>
+              Hoshino Media works with creators on YouTube, Instagram, and
+              TikTok who tell stories about life, society, and love.
+              <br /> We typically partner with creators with over 50k followers
+              / subscribers, who tell stories to an engaged audience.
+            </p>
+            <p>
+              Our talent is diverse across gender, ethnicity and race,
+              orientation, and even hair texture 💇‍♀️
+            </p>
+            <br />
+            <h3>5. What is your pricing?</h3>
+            <p>
+              For creator partnerships, we operate on a commission basis. <br />
+              Our commission is 20% of the total budget.
+            </p>
+            <br />
+            <a
+              href="https://calendly.com/nathanblee/hoshino-media-discovery-call"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-custom brand btn-lg page-scroll"
+            >
+              Book a Call
+            </a>{" "}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
