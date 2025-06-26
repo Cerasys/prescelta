859adb7f2cbe174bec7e97461274d34f3a486b51
Nathan <nathanblee@gmail.com>
2025-06-26 14:04:43 -0700

    initial creator hero image

--- src/components/creators.jsx
+++ src/components/creators.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 import MarketingHighlights from "./marketingHighlights";
+import "./creators.css";
 
 export const Creators = () => {
   return (
-    <div className="body">
+    <div className="creators">
+      <div className="creators-video-container">
+        <video
+          autoPlay // Start playing automatically
+          loop // Loop the video
+          muted // Crucial for autoplay to work in most modern browsers
+          playsInline // Important for iOS Safari to play inline rather than fullscreen
+          className="creators-background-video" // CSS class for styling the video
+          poster="/img/editing-poster.jpg"
+        >
+          <source src="/img/editing.mp4" type="video/mp4" />
+        </video>
+        <div className="creators-video-overlay">
+          <div className="row">
+            <div className="col-md-8 col-md-offset-2 intro-text">
+              <h1>
+                You are a{" "}
+                <span className="highlight text-shimmer"> Storyteller,</span>{" "}
+                Not a<span className="highlight"> Number</span>
+              </h1>
+              <br />
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
       <MarketingHighlights />
       <a
         href="https://calendly.com/hoshinomedia/discovery"
