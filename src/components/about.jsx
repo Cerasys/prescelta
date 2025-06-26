305cf09f76d6029b85cd27a090d62a7015a1112d
Nathan <nathanblee@gmail.com>
2025-06-25 19:08:20 -0700

    completed about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,67 +1,27 @@
 import React from "react";
+import "./about.css";
+import MarkdownContent from "./markdownContent";
 
 export const About = () => {
   return (
-    <div id="about">
+    <div id="about" className="about">
       <div className="container">
         <div className="row">
-          <div className="col-xs-12 col-md-6">
-            {" "}
-            <img
-              src="img/team/nathan.jpg"
-              className="img-responsive"
-              alt=""
-            />{" "}
-          </div>
-          <div className="col-xs-12 col-md-6">
+          <div className="col-xs-12 col-md-12">
             <div className="about-text">
               <h2>Our Story</h2>
-              <p>
-                I'm Nathan, your{" "}
-                <b>influence architect and marketing engineer</b>. I cut my
-                teeth in ecommerce, <u>first for survival, then for fun.</u>
-                <br />
-                <br />
-                In 2020, I was a code monkey that went rogue and started his own
-                brand, and then instantly got <i>dropkicked by Covid.</i>
-                <br />
-                <br />
-                I was making organic content but barely saw any progress.
-                <br /> I knew I needed to market, but had no idea how.
-                <br />
-                <br />
-                I wasted my first $300 on paid ads, with one sale.
-                <br />I wasted the next $300, no sales.
-                <br />
-                We sent some products to some friends for them to post about and
-                got some user testimonials. Two sales. <br />
-                <b>Progress.</b>
-                <br />
-                <br />I kept cranking until from zero until I saw{" "}
-                <b>
-                  $11,000 show up overnight in my bank account and
-                  <i> I knew I had something.</i>
-                </b>
-                <br />
-                <br />
-                I've gone all in on marketing, influencers, and ads since then,
-                first for me, then for my friends, and then for strangers
-                brought to me by the universe.
-                <br />
-                <br />
-                Influence is a funny thing.
-                <br />{" "}
-                <b>
-                  We all have people in our life who have impact on our lives...{" "}
-                </b>
-                <br />
-                people we pay attention to, who hold weight in their opinions
-                and emotions.
-                <br />
-                <br />I want to show you how Influencer Marketing is more than
-                just a channel, <b>it's an engine for growth.</b>
-                <br />
-              </p>
+              <MarkdownContent markdownPath="/content/about1.md" />
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-xs-12 col-md-12">
+            <div className="about-text">
+              <MarkdownContent markdownPath="/content/about2.md" />
+            </div>
+            <div className="about-text">
+              <MarkdownContent markdownPath="/content/about3.md" />
               <h3>
                 So if you're ready to leverage Influencer Marketing to help your
                 Organic, Paid Media, Retail, Customer Experience, and Your
