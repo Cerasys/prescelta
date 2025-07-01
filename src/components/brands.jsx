bf17c479ce4b6f132127d608bdafa9b4bc23d711
Nathan <nathanblee@gmail.com>
2025-06-30 21:43:08 -0700

    v1 of brand updates with video and all

--- src/components/brands.jsx
+++ src/components/brands.jsx
@@ -1,14 +1,36 @@
 import React from "react";
+import "./brands.css";
+import Testimonials from "./testimonials/testimonials";
 
 export const Brands = () => {
   return (
-    <a
-      href="https://calendly.com/nathanblee/hoshino-media-discovery-call"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="btn btn-brand btn-lg page-scroll"
-    >
-      I'm a Brand, Show me How!
-    </a>
+    <div className="brand">
+      <div className="brand-video-container">
+        <video
+          autoPlay // Start playing automatically
+          loop // Loop the video
+          muted // Crucial for autoplay to work in most modern browsers
+          playsInline // Important for iOS Safari to play inline rather than fullscreen
+          className="brand-background-video" // CSS class for styling the video
+          poster="/img/color-poster.jpg"
+        >
+          <source src="/img/color-grading.mp4" type="video/mp4" />
+        </video>
+        <div className="brand-video-overlay">
+          <div className="row">
+            <div className="col-md-8 col-md-offset-2 intro-text">
+              <h1>
+                Partner with{" "}
+                <span className="highlight text"> Authenticity</span>
+              </h1>
+              <br />
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* here you should have a section on brands we've partnered with as well as testimonials */}
+      <Testimonials />
+    </div>
   );
 };
