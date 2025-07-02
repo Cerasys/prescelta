f9132653d0283a51dd320a2110222b7e16a0b526
Nathan <nathanblee@gmail.com>
2025-07-01 19:39:30 -0700

    updated testimonials with photos and bigger names; removed nonused images

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -8,42 +8,42 @@ const TestimonialsSection = () => {
         "The value add is really good. There are programs out there that kind of do false advertising— like once you get into it you realize it's not what you paid for. And it becomes this program you paid for but you don't get the results. But I'm already seeing results and that comes from Nathan's expertise and how well he supports my work.",
       name: "Nitish M",
       title: "Mental Health",
-      //  image: "",
+      image: "/img/testimonials/nitish.jpg",
     },
     {
       quote:
-        "Nathan, I wish I had met you 20 years ago! Consulting with Nathan helped clarify the most strategic options for my marketing. He was very patient, intentional and knowledgeable. He took the time to listen about my business, and he put in the work to generate solutions. I heartily recommend his help to others!",
+        "Nathan, I wish I had met you 20 years ago! Consulting with Nathan helped clarify the most strategic options for me! He was very patient, intentional and knowledgeable. He took the time to listen about my business, and he put in the work to generate solutions. I heartily recommend his help to others!",
       name: "Pretti Shah", // Replace with actual name
       title: "Women's Health", // Replace with actual title
-      // image: "/images/alex-johnson.jpg",
+      image: "/img/testimonials/pretti.jpg",
     },
     {
       quote:
         "I am so grateful to have found Nathan who helped me realize there are ways to gain freedom and they are so readily accessible. I am now less burned out and more passionate about my work than I've ever been before. This is the best thing that has ever happened to me.",
       name: "Jessica W", // Replace with actual name
       title: "Japan Travel", // Replace with actual title
-      // image: "/images/alex-johnson.jpg",
+      image: "/img/testimonials/jessica.jpg",
     },
     {
       quote:
-        "Nathan has a really strong business and tech background. He was able to communicate things in ways that I could easily understand and was never taught about in school. He has a really strong digital marketing foundation, and that was the most helpful lesson I learned to run my business during the program.",
+        "Nathan has a really strong business and tech background. He was able to communicate things in ways that I could easily understand and was never taught about in school. He has a really strong digital marketing foundation, and that was the most helpful lesson I learned to run my business.",
       name: "Linh T", // Replace with actual name
       title: "Parenting", // Replace with actual title
-      //image: "/images/alex-johnson.jpg",
+      image: "/img/testimonials/linh.jpg",
     },
     {
       quote:
         "Nathan's guidance was transformative for my business. His insights into digital marketing are unparalleled, and he genuinely cares about his clients' success. Highly recommend!",
       name: "Isaiah M.",
       title: "Bi-racial Couples",
-      // image: "/images/alex-johnson.jpg",
+      image: "/img/testimonials/isaiah.jpg",
     },
     {
       quote:
         "I was struggling with my online presence, but Nathan provided a clear roadmap and the tools I needed to thrive. My engagement has skyrocketed!",
       name: "Cindy A.",
       title: "Beauty and Spirituality",
-      // image: "/images/alex-johnson.jpg",
+      image: "/img/testimonials/anon.png",
     },
   ];
 
