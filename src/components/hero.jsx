131a5cb1d85e03f3acacfde776a138b152604267
Nathan <nathanblee@gmail.com>
2025-07-01 19:26:50 -0700

    hero copy changes

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -8,16 +8,16 @@ export const Hero = () => {
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                <h4>influencer marketing ...</h4>
+                <h4>influencer marketing for authentic storytellers</h4>
                 <h1>
-                  Where <span className="highlight"> Integrity</span> Meets
+                  Where <span className="highlight"> Influence</span> Meets
                   <span className="highlight"> Storytelling</span>
                 </h1>
                 <br />
                 <br />
                 <p>
                   We connect high-integrity creators with brands that value
-                  depth, curiosity, and storytelling
+                  depth over fleeting trends
                 </p>
                 <a
                   href="/creators"
