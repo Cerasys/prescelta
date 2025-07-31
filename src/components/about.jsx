74e62e90467ff91fc50aa395fb7f908ba6cce9f3
Nathan <nathanblee@gmail.com>
2025-07-30 18:11:45 -0700

    about css changes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -24,26 +24,17 @@ export const About = () => {
               <MarkdownContent markdownPath="/content/about3.md" />
             </div>
             <div className="btn-container">
-              <h3>
-                So if you're ready to leverage Influencer Marketing to help your
-                Organic, Paid Media, Retail, Customer Experience, and Your
-                Bottom Line...
+              <h3 className="highlight">
+                So if you're ready to leverage <s>Influencer</s> Storytelling
+                Marketing to help people attach to your Brand...
               </h3>
-              <a
-                href="https://calendly.com/hoshinomedia/discovery"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="btn btn-custom btn-lg page-scroll"
-              >
-                I'm a Creator, Show me How!
-              </a>{" "}
               <a
                 href="https://calendly.com/nathanblee/hoshino-media-discovery-call"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="btn btn-brand btn-lg page-scroll"
               >
-                I'm a Brand, Show me How!
+                Book a call!
               </a>{" "}
             </div>
           </div>
