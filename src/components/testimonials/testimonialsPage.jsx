3476f29480596b83cd0d75aa31e8d85ba36a4693
Nathan <nathanblee@gmail.com>
2026-04-27 11:58:27 -0700

    testimonial refresh

--- src/components/testimonials/testimonialsPage.jsx
+++ src/components/testimonials/testimonialsPage.jsx
@@ -7,6 +7,20 @@ import "./testimonials.css"; // Ensure your CSS file is imported
 const testimonials = [
   {
     quote:
+      "Nathan is extremely knowledgable and has helped us identify exactly what we needed to improve.",
+    name: "Jennifer A.",
+    title: "Jewelry",
+    image: "/img/testimonials/anon.png",
+  },
+  {
+    quote:
+      "I was struggling with my online presence, but Nathan provided a clear roadmap and the tools I needed to thrive. My engagement has skyrocketed!",
+    name: "Cindy A.",
+    title: "Beauty",
+    image: "/img/testimonials/anon.png",
+  },
+  {
+    quote:
       "The value add is really good. There are programs out there that kind of do false advertising— like once you get into it you realize it's not what you paid for. And it becomes this program you paid for but you don't get the results. But I'm already seeing results and that comes from Nathan's expertise and how well he supports my work.",
     name: "Nitish M",
     title: "Mental Health",
@@ -16,36 +30,22 @@ const testimonials = [
     quote:
       "Nathan, I wish I had met you 20 years ago! Consulting with Nathan helped clarify the most strategic options for me! He was very patient, intentional and knowledgeable. He took the time to listen about my business, and he put in the work to generate solutions. I heartily recommend his help to others!",
     name: "Pretti Shah", // Replace with actual name
-    title: "Women's Health", // Replace with actual title
+    title: "Mental Health", // Replace with actual title
     image: "/img/testimonials/pretti.jpg",
   },
   {
     quote:
       "I am so grateful to have found Nathan who helped me realize there are ways to gain freedom and they are so readily accessible. I am now less burned out and more passionate about my work than I've ever been before. This is the best thing that has ever happened to me.",
     name: "Jessica W", // Replace with actual name
-    title: "Japan Travel", // Replace with actual title
+    title: "Mental Health", // Replace with actual title
     image: "/img/testimonials/jessica.jpg",
-  },
-  {
-    quote:
-      "Nathan has a really strong business and tech background. He was able to communicate things in ways that I could easily understand and was never taught about in school. He has a really strong digital marketing foundation, and that was the most helpful lesson I learned to run my business.",
-    name: "Linh T", // Replace with actual name
-    title: "Parenting", // Replace with actual title
-    image: "/img/testimonials/linh.jpg",
   },
   {
     quote:
       "Nathan's guidance was transformative for my business. His insights into digital marketing are unparalleled, and he genuinely cares about his clients' success. Highly recommend!",
     name: "Isaiah M.",
-    title: "Bi-racial Couples",
+    title: "Mental Health",
     image: "/img/testimonials/isaiah.jpg",
-  },
-  {
-    quote:
-      "I was struggling with my online presence, but Nathan provided a clear roadmap and the tools I needed to thrive. My engagement has skyrocketed!",
-    name: "Cindy A.",
-    title: "Beauty and Spirituality",
-    image: "/img/testimonials/anon.png",
   },
 ];
 
@@ -130,7 +130,9 @@ const TestimonialsPage = () => {
       <div className="testimonials-container">
         <h2 className="testimonials-heading">What Our Clients Say</h2>
         <p className="testimonials-subheading">
-          Hear directly from the creators and entrepreneurs Nathan has helped.
+          I used to work largely in Mental Health doing marketing for therapists
+          before moving towards ecommerce. Here are what my past clients have
+          said.
         </p>
 
         {/* Horizontal scroll container */}
