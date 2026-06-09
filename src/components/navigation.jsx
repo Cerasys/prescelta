20e1ddf127449b4400dff56e5ba3752c80e5b728
Nathan <nathanblee@gmail.com>
2026-06-08 19:00:11 -0700

    navigation update

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,69 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Navigation = (props) => {
+  // 1. Manage the menu collapse state natively in React
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // 2. Helper function to close the menu when a link is clicked
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
+  };
+
+  // 3. Helper function to toggle the menu state when hitting the hamburger button
+  const handleToggleClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav id="menu" className="navbar navbar-default navbar-fixed-top">
       <div className="container">
         <div className="navbar-header">
           <button
             type="button"
-            className="navbar-toggle collapsed"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
+            // Use standard conditional class strings to toggle formatting look dynamically
+            className={`navbar-toggle ${isMenuOpen ? "" : "collapsed"}`}
+            onClick={handleToggleClick}
           >
-            {" "}
-            <span className="sr-only">Toggle navigation</span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
-            <span className="icon-bar"></span>{" "}
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
           </button>
-          <Link className="navbar-brand page-scroll logo" to="/#page-top">
+
+          <Link
+            className="navbar-brand page-scroll logo"
+            to="/#page-top"
+            onClick={handleLinkClick} // Closes menu if brand logo is clicked
+          >
             <img
               src="img/logo_juicy.png"
               alt="Juicy Logo"
               style={{ maxWidth: "80px" }}
             />
-          </Link>{" "}
+          </Link>
         </div>
 
+        {/* 4. Append 'in' conditionally so modern UI transitions process seamlessly */}
         <div
-          className="collapse navbar-collapse"
+          className={`collapse navbar-collapse ${isMenuOpen ? "in" : ""}`}
           id="bs-example-navbar-collapse-1"
         >
           <ul className="nav navbar-nav navbar-right">
             <li>
-              {/* <Link to="/#testimonials" className="page-scroll">
-                Testimonials
-              </Link> */}
-            </li>
-            <li>
-              <Link to="/about" rel="noopener noreferrer">
+              <Link to="/about" onClick={handleLinkClick}>
                 Our Story
               </Link>
             </li>
             <li>
-              <Link to="/brands" rel="noopener noreferrer">
+              <Link to="/brands" onClick={handleLinkClick}>
                 Services
               </Link>
             </li>
-            {/* <li>
-              <Link to="/creators" rel="noopener noreferrer">
-                Creators
-              </Link>
-            </li> */}
             <li>
-              <Link to="/testimonials" rel="noopener noreferrer">
+              <Link to="/testimonials" onClick={handleLinkClick}>
                 Results
               </Link>
             </li>
             <li>
-              <Link to="/faq" rel="noopener noreferrer">
+              <Link to="/faq" onClick={handleLinkClick}>
                 FAQ
               </Link>
             </li>
             <li>
-              <Link to="/resources" rel="noopener noreferrer">
+              <Link to="/resources" onClick={handleLinkClick}>
                 Resources
               </Link>
             </li>
